2316d8a987e5f3fdb2ed82ce3264e1dd03937b6a
Joel <103879u@gmail.com>
2017-01-21 20:54:13 +0800

    Fixed x y overlay DAO BENG dum balek. Next to take into account border, padding. Overlay ears to move further apart from nose

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
             return "https:" + url
         return url
     }
-    function overlayImage($imgContainer, face){
+    function overlayImage($imgContainer, $img, face){
         dogifyLog(face)
         var faceLandmarks = face['faceLandmarks']
         var $overlayImg = $("<img>")
@@ -18,24 +18,36 @@ $(document).ready(function(){
         var nose = faceLandmarks['noseTip']
         var lip = faceLandmarks['upperLipBottom']
 
+        var dogPng = {
+            height : 960
+        }
         //get the height ratio
         var ratio = (lip.y - nose.y) / 110;
 
         //mount up dog pic at coordinates:
+        // natural
         var x = nose.x - (330 * ratio)
         var y = nose.y - (480 * ratio)
+        var height = dogPng['height'] * ratio
+
+        // displayed
+        var naturalHeightRatio = ($img.height() / $img[0].naturalHeight)
+        x = x * naturalHeightRatio
+        y = y * ($img.width() / $img[0].naturalWidth)
+        height = height * naturalHeightRatio
 
         dogifyLog('ratio : ' + ratio)
+        dogifyLog('display ratio : ' + naturalHeightRatio)
         dogifyLog('x : ' + x)
         dogifyLog('y : ' + y)
-        dogifyLog('height : ' + 960 * ratio)
+        dogifyLog('height : ' + height)
 
         $overlayImg.attr('src',  chrome.extension.getURL('img/overlays/dog.png'))
             .css({
                 'position' : 'absolute',
-                'top' : x + 'px',
-                'left' : y + 'px',
-                'height' : 960 * ratio,
+                'top' : y + 'px',
+                'left' : x + 'px',
+                'height' : height,
                 'display' : 'none'
             })
             .on('load', function(){
@@ -44,35 +56,39 @@ $(document).ready(function(){
         $imgContainer.append($overlayImg)
     }
     function putPNGsOnTop(params){
-        var $originalImgContainer = $(params['original_img_container_selector'])
-        var $img = $(params['original_img_selector'])
-        var imgUrl = sanitizeImgUrl($img.attr('src'))
-        dogifyLog('URL from ' + params['original_img_selector'] + " : " + imgUrl)
-        getFaceResults(imgUrl).then(function(faceResults){
-            dogifyLog('Successful request to Cognition API')
-            dogifyLog(faceResults.length + ' faces')
-            faceResults.forEach(function(face, idx){
-                dogifyLog('Overlaying ' + (idx + 1) + '/' + faceResults.length)
-                overlayImage($originalImgContainer, face)
+        var $imgs = $(params['original_img_selector'])
+        $imgs.each(function(_, img){
+            var $img = $(img);
+            var $originalImgContainer = $img.parent();
+            var imgUrl = sanitizeImgUrl($img.attr('src'))
+            dogifyLog('URL from ' + params['original_img_selector'] + " : " + imgUrl)
+            getFaceResults(imgUrl).then(function(faceResults){
+                dogifyLog('Successful request to Cognition API')
+                dogifyLog(faceResults.length + ' faces')
+                faceResults.forEach(function(face, idx){
+                    dogifyLog('Overlaying ' + (idx + 1) + '/' + faceResults.length)
+                    overlayImage($originalImgContainer, $img, face)
+                })
+            }, function(err){
+                dogifyLog('getFaceResults() error')
+                dogifyLog(err)
             })
-        }, function(err){
-            dogifyLog('getFaceResults() error')
-            dogifyLog(err)
         })
     }
 
     var targets = {
         facebook_profile : {
-            original_img_selector : ".profilePic.img",
-            original_img_container_selector : ".photoContainer .profilePicThumb"
+            original_img_selector : ".profilePic.img"
         },
         facebook_verified_profile : {
             original_img_selector : "",
             original_img_container_selector : ""
         },
         wikipedia_main : {
-            original_img_selector : ".vcard tr:nth-child(2) .image img",
-            original_img_container_selector : ".vcard tr:nth-child(2) td a"
+            original_img_selector : ".vcard tr:nth-child(2) .image img:first-child"
+        },
+        localhost_test : {
+            original_img_selector : ".imgContainer img"
         }
     }
 
